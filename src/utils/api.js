db41f1a0cd494cafb9e23c581a3550f28aea32a5
Anton Hansson <125166515+antxhan@users.noreply.github.com>
2025-01-06 17:40:08 +0100

    feat: get category max questions + bug fix if no categories exist in cache

--- src/utils/api.js
+++ src/utils/api.js
@@ -11,13 +11,34 @@ export const api = {
   },
   async categories() {
     const cachedCategories = db.getCategories();
-    if (cachedCategories && cachedCategories?.trivia_categories.length > 0) {
+    console.log(cachedCategories);
+    if (cachedCategories && cachedCategories?.trivia_categories?.length > 0) {
       return cachedCategories;
     } else {
       const endpoint = "/api_category.php";
       const categories = await this._request(endpoint);
       db.setCategories(categories);
       return categories;
+    }
+  },
+  async categoryMaxQuestions(categoryId) {
+    const endpoint = `/api_count.php?category=${categoryId}`;
+    const cachedCategories = db.getCategories();
+    if (cachedCategories) {
+      const category = cachedCategories.trivia_categories.find(
+        (category) => category.id === parseInt(categoryId)
+      );
+      if (category && category.question_count) {
+        return category.question_count;
+      } else {
+        const questionCount = await this._request(endpoint);
+        category.question_count = questionCount.category_question_count;
+        db.setCategories(cachedCategories);
+        return questionCount.category_question_count;
+      }
+    } else {
+      const questionCount = await this._request(endpoint);
+      return questionCount.category_question_count;
     }
   },
   quiz({
