adbab3f4f9da8d5e9b363c0720e1f9420264caad
Anton Hansson <125166515+antxhan@users.noreply.github.com>
2025-01-07 11:37:18 +0100

    style: colors on quiz answers and score label font size

--- src/pages/quiz/components/Score.js
+++ src/pages/quiz/components/Score.js
@@ -1,8 +1,8 @@
 export function Score(amount, correct) {
   return `
       <div class="score">
-        <span>Score</span>
-        <span class="score__amount">${correct} / ${amount}</span>
+      <span class="score__label">Score</span>
+      <span class="score__amount">${correct} / ${amount}</span>
       </div>
       `;
 }
